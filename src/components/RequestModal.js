fdc95a54d4a4a0d0f2a514a445f4395e04273364
Ivan M <crf125@rocketmail.com>
2022-02-10 14:37:53 -0800

    Added request and cancel button to request day modal

--- src/components/RequestModal.js
+++ src/components/RequestModal.js
@@ -6,7 +6,18 @@ export default function RequestModal({ closeModal }) {
     <div className="modal-backdrop">
       <div className="modal">
         <h2>Select a Date to Request</h2>
-        <button onClick={closeModal}>close</button>
+
+        <form>
+            <label style={{textAlign: "left"}}>
+                <span>Date:</span>
+                <input type = "date"/>
+            </label>
+
+            <div style={{display: "inline-flex"}}>
+                <button style={{marginRight: "5px"}} onClick={closeModal}>Request</button>
+                <button style={{marginLeft: "5px"}} onClick={closeModal}>Cancel</button>
+            </div>
+        </form>
       </div>
     </div>
   );
