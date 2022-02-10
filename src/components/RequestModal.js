5ea9efb5d0c085470c4b10fa5eaea7a9bbfe1098
Ivan M <crf125@rocketmail.com>
2022-02-10 14:43:05 -0800

    Added styling to request a date modal buttons and input field

--- src/components/RequestModal.js
+++ src/components/RequestModal.js
@@ -10,12 +10,12 @@ export default function RequestModal({ closeModal }) {
         <form>
             <label style={{textAlign: "left"}}>
                 <span>Date:</span>
-                <input type = "date"/>
+                <input type = "date" style={{borderColor: '#8D69F1'}}/>
             </label>
 
             <div style={{display: "inline-flex"}}>
-                <button style={{marginRight: "5px"}} onClick={closeModal}>Request</button>
-                <button style={{marginLeft: "5px"}} onClick={closeModal}>Cancel</button>
+                <button className = "btn" style={{marginRight: "5px"}} onClick={closeModal}>Request</button>
+                <button className = "btn" style={{marginLeft: "5px"}} onClick={closeModal}>Cancel</button>
             </div>
         </form>
       </div>
