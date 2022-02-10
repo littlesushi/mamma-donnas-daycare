2c6a3d7cbe252e8fcb5bc01880326f99502d4861
Ivan M <crf125@rocketmail.com>
2022-02-10 12:09:41 -0800

    Added useCollection import to AdminHomePage to fix module not found error

--- src/pages/adminHomepage/AdminHomepage.js
+++ src/pages/adminHomepage/AdminHomepage.js
@@ -1,56 +1,58 @@
 // libraries
-import { useState } from "react"
-import { useAuthContext } from '../../hooks/useAuthContext'
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 // context
-import ProjectFilter from './TaskFilter'
+import ProjectFilter from "./TaskFilter";
 
 export default function AdminHomepage() {
-    const { documents, error } = useCollection('users')
-    const [currentFilter, setCurrentFilter] = useState('all')
+  const { documents, error } = useCollection("users");
+  const [currentFilter, setCurrentFilter] = useState("all");
 
-    const changeFilter = (newFilter) => {
-        setCurrentFilter(newFilter)
-    }
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
 
-    const students = documents ? documents.filter((document) => {
-        switch(currentFilter) {
-            case 'all':
-                return true
-            default:
-                return true
+  const students = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          default:
+            return true;
         }
-    }) : null
+      })
+    : null;
 
-    // if return true, keep it, false toss it out
-    // const projects = documents ? documents.filter((document) => {
-    //     switch (currentFilter) {
-    //         case 'all':
-    //             return true
-    //         case 'mine':
-    //             let assignedToMe = false
-    //             document.assignedUsersList.forEach((u) => {
-    //                 if (user.uid === u.id) {
-    //                     assignedToMe = true
-    //                 }
-    //             })
-    //             return assignedToMe
-    //         case 'development':
-    //         case 'design':
-    //         case 'sales':
-    //         case 'marketing':
-    //             console.log(document.category, currentFilter)
-    //             return document.category === currentFilter
-    //         default:
-    //             return true
-    //     }
-    // }) : null
+  // if return true, keep it, false toss it out
+  // const projects = documents ? documents.filter((document) => {
+  //     switch (currentFilter) {
+  //         case 'all':
+  //             return true
+  //         case 'mine':
+  //             let assignedToMe = false
+  //             document.assignedUsersList.forEach((u) => {
+  //                 if (user.uid === u.id) {
+  //                     assignedToMe = true
+  //                 }
+  //             })
+  //             return assignedToMe
+  //         case 'development':
+  //         case 'design':
+  //         case 'sales':
+  //         case 'marketing':
+  //             console.log(document.category, currentFilter)
+  //             return document.category === currentFilter
+  //         default:
+  //             return true
+  //     }
+  // }) : null
 
-    return(
-        <div>
-            <h1>Hello Donna!</h1>
-            < ProjectFilter />
-        </div>
-    )
-    
+  return (
+    <div>
+      <h1>Hello Donna!</h1>
+      <ProjectFilter />
+    </div>
+  );
 }
