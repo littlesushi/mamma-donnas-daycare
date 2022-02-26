eabb42ced88ed16cd7257421f3743c6dee52757e
Ivan M <crf125@rocketmail.com>
2022-02-26 12:03:45 -0800

    Requests page now receives requests firestore

--- src/pages/requestPage/Requests.js
+++ src/pages/requestPage/Requests.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { projectFirestore } from "../../firebase/config";
 
 export default function Requests() {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    projectFirestore
+      .collection("requests")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("No pending schedule requests");
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+
+          setData(results);
+        }
+
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Requests Page</div>
-  )
+    <div>
+      <h2>Requested Days</h2>
+
+      {isLoading && <h2>Loading schedule requests</h2>}
+
+      {error && <p>{error}</p>}
+
+      <Link to="../Home">
+        <button style={{ marginTop: "10px" }} className="btn">
+          Return
+        </button>
+      </Link>
+
+    </div>
+  );
 }
