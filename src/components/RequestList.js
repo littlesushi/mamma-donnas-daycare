87ccb71c99a4d3977c3705d27f1abe7cab0cbe09
Ivan M <crf125@rocketmail.com>
2022-02-26 15:45:26 -0800

    Changed formatting of displayed schedule request in request list items

--- src/components/RequestList.js
+++ src/components/RequestList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./RequestList.css";
 
+const WEEK_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const MONTHS = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+
 export default function RequestList({ requestList }) {
   return (
     <div>
       {requestList.length > 0 && requestList.map((doc) => (
         <div className="request-card">
           <h3 style={{textAlign: 'left'}}>Schedule Request by ...</h3>
-          <p style={{textAlign: 'left', marginTop: '2px'}}>Date requested: {doc.request_date}</p>
+          <p style={{textAlign: 'left', marginTop: '2px'}}>Date requested: {getStringDateFormat(doc.request_date)}</p>
 
           <div style={{ display: "flex" }}>
             <button
@@ -29,4 +35,12 @@ export default function RequestList({ requestList }) {
       ))}
     </div>
   );
+}
+
+function getStringDateFormat(date){
+  const d = new Date(date);
+
+  //Add 1 to the current month as javascript returns a 0 based month value
+  const stringDateFormat = WEEK_DAYS[d.getDay()] + " " +MONTHS[d.getMonth()] + " " +(d.getMonth()+1);
+  return stringDateFormat;
 }
