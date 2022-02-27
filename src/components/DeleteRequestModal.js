25659488bfb6a3f610a861f9ff022a8cbab675d1
Ivan M <crf125@rocketmail.com>
2022-02-26 23:06:10 -0800

    Added functionality to decline button for declining a schedule request

--- src/components/DeleteRequestModal.js
+++ src/components/DeleteRequestModal.js
@@ -1,15 +1,28 @@
 import React from "react";
 
-export default function DeleteRequestModal({ closeModal, clearDocId, docId}) {
+import { projectFirestore } from "../firebase/config";
+
+export default function DeleteRequestModal({ closeModal, clearDocId, docId }) {
   return (
     <div className="modal-backdrop">
       <div className="modal">
         <h2>Are you sure you want to decline this request?</h2>
 
-        <textarea style= {{resize:'none', marginTop: '2px'}} placeholder="Send a (optional) message to the requestee."/>
+        <textarea
+          style={{ resize: "none", marginTop: "2px" }}
+          placeholder="Send a (optional) message to the requestee."
+        />
 
-        <div style={{ display: "inline-flex", marginTop: '2px'}}>
-          <button className="btn" style={{ marginRight: "5px" }}>
+        <div style={{ display: "inline-flex", marginTop: "2px" }}>
+          <button
+            className="btn"
+            style={{ marginRight: "5px" }}
+            onClick={() => {
+              projectFirestore.collection('requests').doc(docId).delete();
+              clearDocId();
+              closeModal();
+            }}
+          >
             Decline
           </button>
 
