b1942597cde7c59c44b435780d48bf0974f2edeb
Ivan M <crf125@rocketmail.com>
2022-03-14 11:43:31 -0700

    Fixed signup code page css affecting other pages.

--- src/pages/SignupCodes/SignupCodes.js
+++ src/pages/SignupCodes/SignupCodes.js
@@ -4,7 +4,6 @@ import "./SignupCodes.css";
 import { projectFirestore } from "../../firebase/config";
 
 export default function SignupCodes() {
-  
   //State used to update customer and admin sign up codes
   const [adminCode, setAdminCode] = useState(null);
   const [customerCode, setCustomerCode] = useState(null);
@@ -15,57 +14,56 @@ export default function SignupCodes() {
 
   //Request a snapshot of the current codes from firestore
   useEffect(() => {
-
-    const unsub = projectFirestore.collection('SignupCodes').doc('codes').onSnapshot((doc) => {
-    
-      if(doc.exists){
-        const data = doc.data()
-        setCurrAdminCode(data.admin_code);
-        setCurrCustomerCode(data.customer_code);
-      }
-      else{
-        setCurrAdminCode("Could not find current admin sign up code");
-        setCurrCustomerCode("Could not find current customer sign up code");
-      }
-    });
+    const unsub = projectFirestore
+      .collection("SignupCodes")
+      .doc("codes")
+      .onSnapshot((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          setCurrAdminCode(data.admin_code);
+          setCurrCustomerCode(data.customer_code);
+        } else {
+          setCurrAdminCode("Could not find current admin sign up code");
+          setCurrCustomerCode("Could not find current customer sign up code");
+        }
+      });
 
     return () => unsub();
-  }, ['codes']);
+  }, ["codes"]);
 
   //Update customer sign up code in firestore
   const handleCustomerCode = (e) => {
-
     //Clear input field after submitting
     document.getElementById("change-customer-code-form").reset();
 
     if (customerCode && customerCode.trim() !== "") {
       console.log("calling update");
       projectFirestore.collection("SignupCodes").doc("codes").update({
-        customer_code: customerCode
+        customer_code: customerCode,
       });
     }
   };
 
   //Update admin sign up code in firestore
   const handleAdminCode = (e) => {
-
     //Clear input field after submitting
     document.getElementById("change-admin-code-form").reset();
 
     if (adminCode && adminCode.trim() !== "") {
       console.log("calling update");
       projectFirestore.collection("SignupCodes").doc("codes").update({
-        admin_code: adminCode
+        admin_code: adminCode,
       });
     }
   };
 
   return (
     <div>
-      <h2>Customer Signup Code:</h2>
+      <h2 className="signup-code-header">Customer Signup Code:</h2>
       <p>Current code: {currCustomerCode}</p>
-      <form id="change-customer-code-form">
+      <form class="signup-code-form" id="change-customer-code-form">
         <input
+          className="signup-code-input"
           type="text"
           placeholder="Enter a new code"
           onChange={(e) => setCustomerCode(e.target.value)}
@@ -75,11 +73,12 @@ export default function SignupCodes() {
         Change code
       </button>
 
-      <h2>Admin Signup Code:</h2>
+      <h2 className="signup-code-header">Admin Signup Code:</h2>
       <p>Current code: {currAdminCode}</p>
 
-      <form className="admin-form" id="change-admin-code-form">
+      <form class="signup-code-form" id="change-admin-code-form">
         <input
+          className="signup-code-input"
           type="text"
           placeholder="Enter a new code"
           onChange={(e) => setAdminCode(e.target.value)}
