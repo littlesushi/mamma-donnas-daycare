ee0155be78c938059d5fc69ee7ed47786cb00485
brainburgercombo <94156382+brainburgercombo@users.noreply.github.com>
2022-03-14 23:44:48 -0700

    Added time range filter

--- src/pages/adminHomepage/accounting/transactionList.js
+++ src/pages/adminHomepage/accounting/transactionList.js
@@ -1,16 +1,46 @@
 import styles from './accounting.module.css'
 import { useState } from 'react'
-export default function TransactionList({transaction, search,searchMode}){
+import { timestamp } from '../../../firebase/config';
+
+function createRow(item, time){
+    var amountEntry = typeof(item.Amount) == 'string' ? item.Amount : String(item.Amount)
+    var isExpense = amountEntry.startsWith("-")
+    
+    return (
+        <tr className = { styles.row}> 
+            <td className = {styles.cell} >
+                {(time.getMonth() + 1 )+ '/' + time.getDate() + '/' + time.getFullYear()}
+            </td>
+            <td className = {styles.cell} >
+                {item.Description}
+            </td>
+            <td className= {styles.cell}>
+                {isExpense?  'Expense' : 'Income'}
+            </td>
+            <td className = {styles.cell}>
+                {item.Name}
+            </td>
+            <td className = { isExpense ? styles.cellExpense : styles.cell}>
+                {isExpense  ? "-$" + amountEntry.substring(1):  '$' + amountEntry}
+            </td>
+        </tr>);
+}
+export default function TransactionList({transaction, search,searchMode, startDateQuery, endDateQuery}){
     var total = 0
     var [sortDate, setSortDate] = useState('1') // 1 = for Ascending Order, -1 for Descending Order
     var [sortDescription, setSortDescription] = useState('1')
     var [sortName, setSortName] = useState('1')
     var [sortAmount, setSortAmount] = useState('1')
-
+    var dateQuery = false
+    
+    
     if(transaction != null){
         total = ( transaction.reduce((total,currentItem) => total = total + parseFloat(currentItem.Amount), 0)).toFixed(2);
     }
-
+   
+    if(!transaction ){
+        return (null)
+    }
     function sortTable(compare,key){
         transaction.sort((a,b) =>{ 
             if(a[key]<b[key]){ 
@@ -39,34 +69,22 @@ export default function TransactionList({transaction, search,searchMode}){
      </tr>
      
     
-    {transaction != null ? transaction.map((item)=>{
-           
-           if(item[searchMode].match(search)){
-                var timeStamp = new Date(item.Date.seconds * 1000 )
-                var amountEntry = typeof(item.Amount) == 'string' ? item.Amount : String(item.Amount)
-                var isExpense = amountEntry.startsWith("-")
-            return (
-            <tr className = { styles.row}> 
-                <td className = {styles.cell} >
-                    {timeStamp.getMonth() + '/' + timeStamp.getDate() + '/' + timeStamp.getFullYear()}
-                </td>
-                <td className = {styles.cell} >
-                    {item.Description}
-                </td>
-                <td className= {styles.cell}>
-                    {isExpense?  'Expense' : 'Income'}
-                </td>
-                <td className = {styles.cell}>
-                    {item.Name}
-                </td>
-                <td className = { isExpense ? styles.cellExpense : styles.cell}>
-                    {isExpense  ? "-$" + amountEntry.substring(1):  '$' + amountEntry}
-                </td>
-    
-            </tr>)
-           }
+    {transaction != null ? transaction.map((entry)=>{
+           dateQuery = (!(isNaN(startDateQuery)) && !(startDateQuery == '')) &&   (!(isNaN(endDateQuery)) && !(endDateQuery == ''))
+           var timeStamp = new Date(entry.Date.seconds * 1000 )
 
+          
+           if(entry[searchMode].match(search)  && !(dateQuery)){
+                return(createRow(entry,timeStamp))
+           }
+           
+           if(entry[searchMode].match(search)  && (dateQuery)){
+               if( startDateQuery<= timeStamp && timeStamp<= endDateQuery){
+                return(createRow(entry,timeStamp))
+               }
+            }
     }): false}
+
     </table>
     </div>)
 
