6026ec4041426fd83a6391f0337bd18b20528de9
littlesushi <36653040+littlesushi@users.noreply.github.com>
2022-03-15 18:38:55 -0800

    Updates to Billing Page THEN-222

--- src/pages/billingPage/Billing.js
+++ src/pages/billingPage/Billing.js
@@ -1,37 +1,33 @@
 // libraries
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { projectFirestore } from "../../firebase/config";
+import { useFirestore } from '../../hooks/useFirestore'
+import { useCollection } from "../../hooks/useCollection";
+import FetchBilling from "./FetchBilling";
+// import { collection, query, limit, where } from "firebase/firestore";
+import { firestore } from "firebase";
 
 // styles
 // import './CustomerHomepage.module.css'
 
 
 export default function BillingPage() {
-    const { authIsReady, user } = useAuthContext() 
-    var days = 0;
-
-    projectFirestore.collection("AttendanceLog").onSnapshot(snapshot => {
-        console.log(`Received doc snapshot: ${snapshot}`);
-        days++;
-        // snapshot.forEach(doc => {
-        //     console.log(doc.id, '=>', doc.data());
-        // });
-    }, err => {
-        console.log(`Encountered error: ${err}`);
-    });
+    var dues = 0;
+    const { authIsReady, user } = useAuthContext()
+    const { documents, error } = useCollection("AttendanceLog");
         
     return (
         <div>
-            <h2>Your Dues:</h2>
-            <h1>{days}</h1>
+            <h2>Total Dues:</h2>
+            <h1>${dues}</h1>
             <div classname="subtext">Due Dec 31 2021</div>
             
             <button className="btn">
                 Pay Now
             </button>
+            <hr class="solid"/>
 
-            <div>Unpaid Days</div>
-            <div>Paid Days</div>
+            <FetchBilling collection={documents} uid={user.uid}/>
         </div>
     )
 }
