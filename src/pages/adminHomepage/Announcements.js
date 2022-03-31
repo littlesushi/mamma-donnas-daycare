9ff3242fc280244c898e8e6e3e4c0fd833ba8ef3
Ivan M <crf125@rocketmail.com>
2022-03-30 17:03:20 -0700

    Added basic UI for making announcements from the admin page

--- src/pages/adminHomepage/Announcements.js
+++ src/pages/adminHomepage/Announcements.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import "./Announcements.css";
 
 export default function Announcements() {
-  return (
-    <div>Announcements</div>
-  )
+	return (
+		<div className="announcments-div">
+			<h2>Make an Announcement</h2>
+
+			<form className="input-form">
+				<input
+					placeholder="Title"
+					type="text"
+					className="title-input"
+				>
+                </input>
+			</form>
+			<textarea className="text-area" rows={12} />
+            <div>
+                <button className="btn">Send Announcement</button>
+            </div>
+		</div>
+	);
 }
