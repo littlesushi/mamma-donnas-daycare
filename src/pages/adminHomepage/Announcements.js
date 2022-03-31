ad86e71e2f81930fdbfc1ec12dfd01cbd5a2048a
Ivan M <crf125@rocketmail.com>
2022-03-30 22:51:59 -0700

    Announcements are now save to a collection in firestore

--- src/pages/adminHomepage/Announcements.js
+++ src/pages/adminHomepage/Announcements.js
@@ -1,24 +1,63 @@
 import React from "react";
 
+import { useState, useEffect } from "react";
+
 import "./Announcements.css";
 
+import { projectFirestore } from "../../firebase/config";
+
 export default function Announcements() {
+	const [title, setTitle] = useState(null);
+	const [body, setBody] = useState(null);
+
+	const handleSubmit = (e) =>{
+
+		//Prevent page from refreshing when submit button is clicked
+		e.preventDefault()
+
+		if(title && body && title !== "" && body != ""){
+
+			console.log("Submitting announcment");
+
+			//Get current date for announcement
+			const today = new Date();
+			const dd = String(today.getDate()).padStart(2, '0');
+			const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+			const yyyy = today.getFullYear();
+
+			//Send announcement to firestore.
+			projectFirestore.collection("Announcements").add({title: title, body: body, date: (mm + '/' + dd + '/' + yyyy) })
+		}
+
+		//Clear entry boxes after clicking the submit button
+		document.getElementById("announcement-title-form").reset();
+		setBody("");
+	}
+
 	return (
 		<div className="announcments-div">
 			<h2>Make an Announcement</h2>
 
-			<form className="input-form">
+			<form className="input-form" id="announcement-title-form">
 				<input
-					placeholder="Title"
+					placeholder="Announcement Title"
 					type="text"
 					className="title-input"
-				>
-                </input>
+					onChange={(e) => setTitle(e.target.value)}
+				></input>
 			</form>
-			<textarea className="text-area" rows={12} />
-            <div>
-                <button className="btn">Send Announcement</button>
-            </div>
+
+			<textarea
+				className="text-area"
+				id="announcement-body-text"
+				placeholder="Announcement Body"
+				rows={12}
+				value={body}
+				onChange={(e) => setBody(e.target.value)}
+			/>
+			<div>
+				<button className="btn" onClick={e => handleSubmit(e)}>Send Announcement</button>
+			</div>
 		</div>
 	);
 }
